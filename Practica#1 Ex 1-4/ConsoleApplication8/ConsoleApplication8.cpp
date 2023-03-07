356a56ce7b19fab2f0fb15a8af1b4f816e192881
M1kr03x <countdownlfg@mail.ru>
2023-03-07 21:39:56 +0300

    Prc

---
+++ Practica#1 Ex 1-4/ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,87 @@
+﻿
+#include <iostream>
+#include <string>
+
+
+class  car {
+    
+public:
+    car(std::string c) {
+        CarName = c;
+    }
+    double maxspeed;
+    double distance;
+    
+   
+    std::string CarName;
+
+
+    int CalculateTime(double t) {
+        t = 0;
+        t = distance / maxspeed;
+        return t;
+        
+    }
+    void nameplusspeed() {
+        std::cout << "Название машины - " << "\t" << CarName << std::endl << "Максимальная скорость - " << "\t" << maxspeed;
+    }
+    void setSpeed(int s) {
+        std::cout << "Введите скорость машины : " << CarName << ": ";
+        std::cin >> s;
+        maxspeed = s;
+
+    }
+    void setDistance(int b) {
+        std::cout << "Введите дистанцию машины : " << CarName << ": ";
+        std::cin >> b;
+        distance = b;
+
+    }
+};
+
+class car2 : public car {
+public:
+    car2(std::string c) : car(c) {
+
+    }
+};
+
+int main()
+{
+    
+    car samara("Mashinka");
+    car FirstCar("Lada");
+    car SecondCar("Ford");
+    car ThirdCar("Zhigul))");
+    car FourthCar("Bnw");
+
+    FirstCar.setSpeed(1);
+    SecondCar.setSpeed(2);
+    ThirdCar.setSpeed(3);
+    FourthCar.setSpeed(4);
+    FirstCar.setDistance(1);
+    SecondCar.setDistance(2);
+    ThirdCar.setDistance(3);
+    FourthCar.setDistance(4);
+    FirstCar.CalculateTime(1);
+    SecondCar.CalculateTime(2);
+    ThirdCar.CalculateTime(3);
+    FourthCar.CalculateTime(4);
+    
+    if (FirstCar.CalculateTime(1) < SecondCar.CalculateTime(1) and FirstCar.CalculateTime(1) < ThirdCar.CalculateTime(1) and FirstCar.CalculateTime(1) < FourthCar.CalculateTime(1)) {
+        std::cout << "Победила " << FirstCar.CarName;
+    }
+    if (SecondCar.CalculateTime(1) < FirstCar.CalculateTime(1) and SecondCar.CalculateTime(1) < ThirdCar.CalculateTime(1) and SecondCar.CalculateTime(1) < FourthCar.CalculateTime(1)) {
+        std::cout << "Победила " << SecondCar.CarName;
+    }
+    if (ThirdCar.CalculateTime(1) < FirstCar.CalculateTime(1) and ThirdCar.CalculateTime(1) < SecondCar.CalculateTime(1) and ThirdCar.CalculateTime(1) < FourthCar.CalculateTime(1)) {
+        std::cout << "Победила " << ThirdCar.CarName;
+    }
+    if (FourthCar.CalculateTime(1) < SecondCar.CalculateTime(1) and FourthCar.CalculateTime(1) < ThirdCar.CalculateTime(1) and FourthCar.CalculateTime(1) < FirstCar.CalculateTime(1)) {
+        std::cout << "Победила " << FourthCar.CarName;
+    }
+    
+    
+
+
+}
---
+++ Practica#2 Ex 1/ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,56 @@
+﻿#include <string>
+#include <Windows.h>
+#include <iostream>
+#include <fstream>
+
+class test {
+private:
+    double first;
+    double second;
+    double third;
+    double discr;
+public:
+    test(double a, double b, double c) {
+        first = a;
+        second = b;
+        third = c;
+    }
+private:double dis(double d) {
+    
+    d = (second * second) - 4 * first * third;
+    discr = d;
+    std::cout << "Дискриминант равен: " << discr;
+    return d;
+}
+public:double CalculateRoots(double x1, double x2) {
+    this->discr = dis(discr);
+    if (discr == 0){
+        x1 = ( - second) / (2 * first);
+        std::cout << std::endl << "Корень равен:" << "x1 = " << x1 << std::endl;
+    }
+    if  (discr < 0) {
+
+        std::cout << std::endl << "Корней нет" << std::endl;
+
+    }
+    else {
+        x1 = (-(second)+std::sqrt(discr)) / 2 * first;
+        x2 = (-(second)-std::sqrt(discr)) / 2 * first;
+        std::cout << std::endl << "Корни равны:" << "x1 = " << x1 << "\t" << "x2 = " << x2 << std::endl;
+
+        return x1, x2;
+    }
+    
+}
+      
+public:double GetDiscr(){
+    this->discr = dis(discr);
+    return discr;
+}
+    };
+    int main()
+    {
+        setlocale(LC_ALL, "Russian");
+        test yr(9,-6,2);
+        yr.CalculateRoots(1,2);
+    };
