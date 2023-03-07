d04e00dd65488fcab10517231f919c62ad27f66a
M1kr03x <countdownlfg@mail.ru>
2023-03-08 00:07:38 +0300

    Chinchanchon

---
+++ Practica#2 Ex 2/ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,60 @@
+﻿
+#include <iostream>
+#include <windows.h>
+#include <string>
+
+
+
+
+
+class test2 {
+private:
+	int n = 0;
+	int a;
+	int number;
+public:
+	bool SetNumber(int number) {
+		
+		if (number == n + 1) {
+
+			n = number;
+			a = number;
+
+			
+			return 1;
+		}
+		else { 
+			a = n + 1;
+			n = 0;
+			
+			return 0; }
+	}
+
+	int PredictNumber() {
+		
+		std::cout << "Ожидаемое значение: ";
+		return a;
+
+	}
+};
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	test2 chelik;
+	
+	std::cout << std::boolalpha << chelik.SetNumber(1) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;;
+	std::cout << std::boolalpha << chelik.SetNumber(2) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;;
+	std::cout << std::boolalpha << chelik.SetNumber(3) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;
+	std::cout << std::boolalpha << chelik.SetNumber(5) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;
+	std::cout << std::boolalpha << chelik.SetNumber(1) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;
+	std::cout << std::boolalpha << chelik.SetNumber(2) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;
+
+	
+	
+};
