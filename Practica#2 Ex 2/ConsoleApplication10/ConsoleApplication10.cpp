0ac64dc709ef8e57e8c7a973cbe361fbfd6e343d
M1kr03x <countdownlfg@mail.ru>
2023-03-08 02:51:55 +0300

    Reload

--- Practica#2 Ex 2/ConsoleApplication10/ConsoleApplication10.cpp
+++ Practica#2 Ex 2/ConsoleApplication10/ConsoleApplication10.cpp
@@ -14,20 +14,24 @@ private:
 	int number;
 public:
 	bool SetNumber(int number) {
-		
+		std::cout << "Введите число: ";
+		std::cin >> number;
 		if (number == n + 1) {
 
 			n = number;
 			a = number;
 
-			
+
 			return 1;
+			
 		}
-		else { 
+		else {
 			a = n + 1;
 			n = 0;
+
+			return 0;
 			
-			return 0; }
+		}
 	}
 
 	int PredictNumber() {
@@ -54,6 +58,32 @@ int main()
 	std::cout << chelik.PredictNumber() << std::endl;
 	std::cout << std::boolalpha << chelik.SetNumber(2) << "\t";
 	std::cout << chelik.PredictNumber() << std::endl;
+	std::cout << std::boolalpha << chelik.SetNumber(1) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;;
+	std::cout << std::boolalpha << chelik.SetNumber(2) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;;
+	std::cout << std::boolalpha << chelik.SetNumber(3) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;
+	std::cout << std::boolalpha << chelik.SetNumber(5) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;
+	std::cout << std::boolalpha << chelik.SetNumber(1) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;
+	std::cout << std::boolalpha << chelik.SetNumber(2) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;
+	std::cout << std::boolalpha << chelik.SetNumber(1) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;;
+	std::cout << std::boolalpha << chelik.SetNumber(2) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;;
+	std::cout << std::boolalpha << chelik.SetNumber(3) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;
+	std::cout << std::boolalpha << chelik.SetNumber(5) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;
+	std::cout << std::boolalpha << chelik.SetNumber(1) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;
+	std::cout << std::boolalpha << chelik.SetNumber(2) << "\t";
+	std::cout << chelik.PredictNumber() << std::endl;
+
+
 
 	
 	
---
+++ Practica#2 Ex 3/ConsoleApplication11/ConsoleApplication11.cpp
@@ -0,0 +1,32 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <locale.h>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const int kol = 100;
+    int i = 0;
+    int sdvig;
+    char symbols[] = { 'А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я','а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я',
+'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','@','!','*','0','1','2','3','4','5','6','7','8','9' };
+    char symbols2[kol];
+    std::cout << "Введите текст умоляю (На строчные английские буквы не хватило бюджета и морали)" << std::endl;
+    std::cin >> symbols2;
+    std::cout << "На сколько символов сдивнуть буквы ну или символы:" << std::endl;
+    std::cin >> sdvig;
+
+    char* begin = symbols;
+    char* end = symbols + sizeof(symbols);
+    for (i=0; symbols2[i]; ++i)
+    {
+        char* finded = std::find(begin, end, symbols2[i]);
+        if (finded != end) symbols2[i] = *(begin + (finded - begin + sdvig) % sizeof(symbols));
+    }
+    std::cout << "Результатыч :  " << std::endl;
+    std::cout << symbols2 << std::endl;
+    return 0;
+}
+
